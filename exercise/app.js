1ad4f3f2976e3216862db7d0c31d4b9d36d8396d
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-10-17 13:50:30 -0400

    game save

--- exercise/app.js
+++ exercise/app.js
@@ -1,18 +1,28 @@
 const express = require('express'),
+    es6Renderer = require('express-es6-template-engine'),
     path = require('path'),
     cookieParser = require('cookie-parser'),
-    logger = require('morgan');
+    logger = require('morgan'),
+    helmet = require('helmet'),
+    compression = require('compression')
 
-// Setting up Routes
+// Setting up Controllers
 const indexController = require('./controllers/index');
 
-var app = express();
+const app = express();
+
+// telling express to use the es6 Templating Engine
+app.engine("html", es6Renderer);
+app.set("views", "./views");
+app.set("view engine", "html");
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+app.use(helmet());
 
 // Telling Express to use the routes
 app.use('/', indexController);
